0cf0768612e354739280a001adcc47738ee598a6
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-03 06:28:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    split = self.split(/[.?!]/)
+    split.delete("")
+    split.count
   end
 end
+
+"This is a string! It has three sentences. Right?".count_sentences
